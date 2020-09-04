2b6c8b2b22c7f03d3cedce25c049ddbdd772e817
mmitch2958 <mikescottmitchell@outlook.com>
2020-09-04 09:39:21 -0400

    moved handel changes to todoform

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
-
-import TodoForm from './components/TodoForm'
-import TodoList from './components/TodoList'
-import './App.css';
-
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 const todos = [
+  {
+    task: "",
+    id: "",
+    completd: false,
+  },
 
-  { task: 'Return your express Jacket', id: 4, completed: false, textDecoration: "none" },
-  { task: 'Go to the Gym', id: 7, completed: false, textDecoration: "none" },
-  { task: 'Watch New Gerard Buttler Movie', id: 8, completed: false, textDecoration: "none" }
-
-]
+  // { task: 'Return your express Jacket', id: 4, completed: false, textDecoration: "none" },
+  // { task: 'Go to the Gym', id: 7, completed: false, textDecoration: "none" },
+  // { task: 'Watch New Gerard Buttler Movie', id: 8, completed: false, textDecoration: "none" }
+];
 
 class App extends React.Component {
   constructor() {
@@ -23,52 +25,35 @@ class App extends React.Component {
     };
   }
 
-  handleChanges = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  addNewToDo = evn => {
-    evn.preventDefault();
+  addNewToDo = (taskName) => {
     this.setState({
       todos: [
         ...this.state.todos,
         {
-          task: this.state.todo,
+          task: taskName,
           id: Date.now(),
           completed: false,
-        }
+        },
       ],
-      todo: ''
     });
-    this.saveList();
   };
 
-  saveList = ev => {
-    const todo = this.state.todo
-    localStorage.setItem('todo', todo);
+  saveList = (ev) => {
+    const todo = this.state.todo;
+    localStorage.setItem("todo", todo);
+  };
 
-  }
-
-  togComp = id => {
-
-    let todos = this.state.todos.map(todo => {
+  togComp = (id) => {
+    let todos = this.state.todos.map((todo) => {
       if (todo.id === id) {
-
-        todo.completd = !todo.completed
-      };
-
-      
-      return todo;
-    
-    })
-
-
-    this.setState({
-      todos,
-      todo: ''
-    })
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todos;
+    });
   };
-
 
   // clearCompleted = () => {
 
@@ -85,28 +70,18 @@ class App extends React.Component {
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
-    console.log('its working!');
+    console.log("its working!");
     return (
-      <div className ='body'>
-        <div className ='App'>
-          <h1 className ='header'>Your TODO List:</h1>
+      <div className="body">
+        <div className="App">
+          <h1 className="header">Your TODO List:</h1>
 
-          <TodoForm
-            saveList={this.saveList}
-            addNewToDo={this.addNewToDo}
-            handleChanges={this.handleChanges}
-            todo={this.state.todo}
-          />
-          <TodoList togComp={this.togComp}
-            todos={this.state.todos}
-          />
+          <TodoForm addNewToDo={this.addNewToDo} />
+          <TodoList togComp={this.togComp} todos={this.state.todos} />
         </div>
-
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,23 +2,44 @@ import React from 'react';
 import './Todo.css'
 
 
-function TodoForm(props) {
-console.log(props.todo)
-    return (
-        <form>
+class TodoForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            newTaskName: ''
+        }
+    }
+    handleChanges = e => {
+        e.preventDefault()
+        this.setState({
+            newTaskName: e.target.value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.addNewTask(this.state.newTaskName)
+        this.setState({
+            newTaskName:""
+        })
+    }
+
+    render() {
+        return(
+        <form onSubmit={this.handleSubmit}>
             <input
-                value={props.target.value}
+                value={this.state.newTaskName}
                 type='text'
                 name='todo'
                 placeholder='Please add Item to list'
-                onChange={props.handelChanges}
+                onChange={this.handelChanges}
             />
             <div className='buttons'>
-                <button onClick={props.addNewItem()}>Add Item To List</button>
-                <button onClick={props.clearCompleted}> Clear List </button>
+                <button>Add Item To List</button>
             </div>
-
         </form>
-    );
-} 
+        )
+    }
+}
+
 export default TodoForm;
